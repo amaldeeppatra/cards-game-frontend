6002525c7edd4a802c2025bf320268cd5b6e204a
Amaldeep <amaldeeppatra21@gmail.com>
2026-01-08 16:10:32 +0530

    .env added

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,10 +1,17 @@
 import { io, Socket } from "socket.io-client";
 
-let socket: Socket;
+let socket: Socket | null = null;
 
 export function getSocket() {
   if (!socket) {
-    socket = io("http://localhost:3001");
+    const url =
+      process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001";
+
+    socket = io(url, {
+      transports: ["websocket"],
+      autoConnect: true
+    });
   }
+
   return socket;
 }
